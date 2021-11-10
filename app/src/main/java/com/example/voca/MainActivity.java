c608bf4fc3d23ea5623eb78575ee6a39b72234b1
ChangHyeonPark <ckdgusqkr0803@gmail.com>
2021-11-11 01:21:40 +0900

    기본적인 UI 구현 v1
    main tab 3, 각 fragment로 구현
    1번 list 탭 구조 구현 완료
    2번, 3번 탭 구현 필요

---
+++ app/src/main/java/com/example/voca/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.voca;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btn1;
+    private Button btn2;
+    private Button btn3;
+
+    private FragmentManager manager; //androidx.fragment.app.FragmentManager
+    private OneFragment oneFragment;
+    private TwoFragment twoFragment;
+    private ThreeFragment threeFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.main_btn1);
+        btn2 = findViewById(R.id.main_btn2);
+        btn3 = findViewById(R.id.main_btn3);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+
+        manager = getSupportFragmentManager();
+        oneFragment = new OneFragment();
+        twoFragment = new TwoFragment();
+        threeFragment = new ThreeFragment();
+
+//      FragmentTransaction tf=manager.beginTransaction();
+//      tf.add(R.id.main_container, oneFragment);
+//      tf.commit();
+
+         FragmentTransaction tf=manager.beginTransaction();
+         tf.add(R.id.main_container, oneFragment);
+         tf.commit();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btn1) {
+            if(!oneFragment.isVisible()) {
+                FragmentTransaction tf = manager.beginTransaction();
+                tf.replace(R.id.main_container, oneFragment);
+                //tf.addToBackStack(null);
+                tf.addToBackStack(null);                        // 실행 스택 쌓음. 뒤로가기 누르면 뒤로 감
+
+                tf.commit();
+            }
+        } else if (v==btn2) {
+            if(!twoFragment.isVisible()) {
+                FragmentTransaction tf = manager.beginTransaction();
+                tf.replace(R.id.main_container, twoFragment);
+                //tf.addToBackStack(null);
+                tf.addToBackStack(null);                        // 실행 스택 쌓음. 뒤로가기 누르면 뒤로 감
+
+                tf.commit();
+            }
+        } else if (v == btn3) {
+            if(!threeFragment.isVisible()) {
+                Log.d("kkang","111111111111");
+                FragmentTransaction tf = manager.beginTransaction();
+                tf.replace(R.id.main_container,threeFragment);
+                //tf.addToBackStack(null);
+                tf.addToBackStack(null);
+
+                tf.commit();
+            }
+        }
+    }
+}
