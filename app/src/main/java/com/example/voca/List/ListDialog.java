da800cd7ec36f0e37d2621f42b4d84a0a29a4760
is-yeop <yeop@yeop.kr>
2021-11-24 16:58:49 +0900

    Add Insert and Livedata Observe Examples
     Changes to be committed:
    	modified:   app/src/main/java/com/example/voca/List/ListDialog.java
    	modified:   app/src/main/java/com/example/voca/MainActivity.java

--- app/src/main/java/com/example/voca/List/ListDialog.java
+++ app/src/main/java/com/example/voca/List/ListDialog.java
@@ -2,21 +2,41 @@ package com.example.voca.List;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.voca.R;
+import com.example.voca.Voca.Voca;
+import com.example.voca.Voca.VocaDao;
+import com.example.voca.Voca.VocaViewModel;
 
 public class ListDialog extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog (Bundle savedInstnanceState) {
+        LayoutInflater inflater=getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.list_dialog, null);
+        VocaViewModel vocaViewModel = new ViewModelProvider(this).get(VocaViewModel.class);
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setIcon(android.R.drawable.ic_dialog_alert);
-        builder.setView(R.layout.list_dialog);
-        builder.setPositiveButton("추가",null);
+        builder.setView(dialogView);
+        builder.setPositiveButton("추가", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                EditText word = dialogView.findViewById(R.id.word_input);
+                EditText mean = dialogView.findViewById(R.id.mean_input);
+                vocaViewModel.insert(new Voca(word.getText().toString(), mean.getText().toString()));
+            }
+        });
         builder.setNegativeButton("취소", null);
         AlertDialog dialog = builder.create();
 
