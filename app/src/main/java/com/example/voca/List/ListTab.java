adce1bf85acfffb41e5a5977beb861f4c283e417
is-yeop <yeop@yeop.kr>
2021-12-03 15:33:10 +0900

    Solve some errors  Changes to be committed: 	modified:   app/src/main/java/com/example/voca/List/ListDialog.java 	modified:   app/src/main/java/com/example/voca/List/ListTab.java

--- app/src/main/java/com/example/voca/List/ListTab.java
+++ app/src/main/java/com/example/voca/List/ListTab.java
@@ -1,11 +1,9 @@
 package com.example.voca.List;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TabHost;
 
@@ -13,12 +11,9 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.voca.R;
 import com.example.voca.Voca.Voca;
-import com.example.voca.Voca.VocaListAdapter;
 import com.example.voca.Voca.VocaViewModel;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -48,7 +43,7 @@ public class ListTab extends Fragment implements View.OnClickListener {
         manager = getFragmentManager();
 
         VocaViewModel vocaViewModel = new ViewModelProvider(this).get(VocaViewModel.class);
-        vocaViewModel.getVocas().observe(this, vocas -> {
+        vocaViewModel.getVocas().observe(getViewLifecycleOwner(), vocas -> {
             vocaList.clear();
             vocaList.addAll(vocas);
         });
@@ -66,16 +61,7 @@ public class ListTab extends Fragment implements View.OnClickListener {
         spec.setContent(R.id.list_tab2);
         tabHost.addTab(spec);
 
-        String[] tmpWords1 = {"A","B","C"};
-        String[] tmpWords2 = {"a","b","c"};
 
-        learningWord = (ListView) tabHost.findViewById(R.id.list_tab1);
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,tmpWords1);
-        learningWord.setAdapter(listViewAdapter);
-
-        learnedWord = (ListView) tabHost.findViewById(R.id.list_tab2);
-        listViewAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,tmpWords2);
-        learnedWord.setAdapter(listViewAdapter);
 
         listFab = (FloatingActionButton) tabHost.findViewById(R.id.list_fab);
         listFab.setOnClickListener(this);
